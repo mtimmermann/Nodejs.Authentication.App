428fbdfa843589b418d35b6cb3f13aaa9c0a9d27
Mark Timmermann <mtimmermann@hotmail.com>
2013-11-27 02:23:45 -0700

    Wirining up registration and logout endpoints.

--- app.js
+++ app.js
@@ -12,10 +12,12 @@ var app = express();
 /*
 Database and Models
 */
-mongoose.connect("mongodb://localhost/myapp");
+mongoose.connect("mongodb://localhost/nodejsauthapp");
 var UserSchema = new mongoose.Schema({
+    email: String,
     username: String,
     password: String,
+    deleted: Boolean,
     salt: String,
     hash: String
 });
@@ -72,8 +74,14 @@ function requiredAuthentication(req, res, next) {
     if (req.session.user) {
         next();
     } else {
-        req.session.error = 'Access denied!';
-        res.redirect('/login');
+        var errorDescription = 'Access denied';
+        req.session.error = errorDescription;
+        res.statusCode = 403;
+        return res.send(JSON.stringify({
+            code: res.statusCode,
+            message: 'Not Authorized',
+            description: errorDescription }));
+        //res.redirect('/login');
     }
 }
 
@@ -93,24 +101,24 @@ function userExist(req, res, next) {
 /*
 Routes
 */
-app.get("/", function (req, res) {
+// app.get("/", function (req, res) {
 
-    if (req.session.user) {
-        res.send("Welcome " + req.session.user.username + "<br>" + "<a href='/logout'>logout</a>");
-    } else {
-        res.send("<a href='/login'> Login</a>" + "<br>" + "<a href='/signup'> Sign Up</a>");
-    }
-});
+//     if (req.session.user) {
+//         res.send("Welcome " + req.session.user.username + "<br>" + "<a href='/logout'>logout</a>");
+//     } else {
+//         res.send("<a href='/login'> Login</a>" + "<br>" + "<a href='/signup'> Sign Up</a>");
+//     }
+// });
 
-app.get("/signup", function (req, res) {
-    if (req.session.user) {
-        res.redirect("/");
-    } else {
-        res.render("signup");
-    }
-});
+// app.get("/signup", function (req, res) {
+//     if (req.session.user) {
+//         res.redirect("/");
+//     } else {
+//         res.render("signup");
+//     }
+// });
 
-app.post("/signup", userExist, function (req, res) {
+app.post("/registration", userExist, function (req, res) {
     var password = req.body.password;
     var username = req.body.username;
 
@@ -118,16 +126,19 @@ app.post("/signup", userExist, function (req, res) {
         if (err) throw err;
         var user = new User({
             username: username,
+            deleted: false,
             salt: salt,
             hash: hash,
         }).save(function (err, newUser) {
             if (err) throw err;
-            authenticate(newUser.username, password, function(err, user){
-                if(user){
+            authenticate(newUser.username, password, function(err, user) {
+                if(user) {
                     req.session.regenerate(function(){
                         req.session.user = user;
-                        req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted">/restricted</a>.';
-                        res.redirect('/');
+                        req.session.success = 'Authenticated as ' + user.username;// + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted">/restricted</a>.';
+                        //res.redirect('/');
+                        return res.send(JSON.stringify({
+                            IsSuccess: true }));
                     });
                 }
             });
@@ -135,9 +146,9 @@ app.post("/signup", userExist, function (req, res) {
     });
 });
 
-app.get("/login", function (req, res) {
-    res.render("login");
-});
+// app.get("/login", function (req, res) {
+//     res.render("login");
+// });
 
 app.post("/login", function (req, res) {
     authenticate(req.body.username, req.body.password, function (err, user) {
@@ -146,8 +157,7 @@ app.post("/login", function (req, res) {
                 req.session.user = user;
                 req.session.success = 'Authenticated as ' + user.username;// + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted">/restricted</a>.';
                 //res.redirect('/');
-                return res.send(JSON.stringify({
-                    IsSuccess: true }));
+                return res.send(JSON.stringify({ IsSuccess: true }));
             });
         } else {
             var errorDescription = 'Authentication failed, please check your username and password.';
@@ -166,13 +176,22 @@ app.post("/login", function (req, res) {
 
 app.get('/logout', function (req, res) {
     req.session.destroy(function () {
-        res.redirect('/');
+        //res.redirect('/');
+        return res.send(JSON.stringify({ IsSuccess: true }));
     });
 });
 
-app.get('/profile', requiredAuthentication, function (req, res) {
-    res.send('Profile page of '+ req.session.user.username +'<br>'+' click to <a href="/logout">logout</a>');
+//app.get('/contacts', function(req, res) {
+app.get('/contacts', requiredAuthentication, function(req, res) {
+    res.send(JSON.stringify({
+        totalRecords: 0,
+        data: []
+    }));
 });
+
+// app.get('/profile', requiredAuthentication, function (req, res) {
+//     res.send('Profile page of '+ req.session.user.username +'<br>'+' click to <a href="/logout">logout</a>');
+// });
 
 
 //http.createServer(app).listen(3000);
