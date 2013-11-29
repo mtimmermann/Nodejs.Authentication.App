6fa762900c607d2d18375f3c63ba8c6087f5ed24
Mark Timmermann <mtimmermann@hotmail.com>
2013-11-28 23:45:03 -0700

    Adding MongoDB transport logger to winston logging config

--- shared/logger.js
+++ shared/logger.js
@@ -1,6 +1,9 @@
 var winston = require('winston'),
     logLevel = require('../config/application.config').logging.level;
 
+//require('winston-riak').Riak;
+require('winston-mongodb').Mongo;
+
 // https://github.com/flatiron/winston
 // http://docs.nodejitsu.com/articles/intermediate/how-to-log
 
@@ -15,8 +18,6 @@ var winston = require('winston'),
 //   error: 5
 // };
 
-//require('winston-riak').Riak;
-//require('winston-mongo').Mongo;
 
 var logger = new (winston.Logger)({
     transports: [
@@ -28,10 +29,10 @@ var logger = new (winston.Logger)({
             maxsize: 20000,
             maxFiles: 20,
             level: logLevel
-        })
+        }),
         //new winston.transports.Couchdb({ 'host': 'localhost', 'db': 'logs' })
         //new winston.transports.Riak({ bucket: 'logs' })
-        //new winston.transports.MongoDB({ db: 'nodejsauthapp_logs', level: 'info'})
+        new winston.transports.MongoDB({ db: 'nodejsauthapp_logs', level: logLevel })
     ],
     exceptionHandlers: [
         new winston.transports.File({ filename: 'logs/exceptions.log' })
